e0306016ad584aeb4a9e15c5165917a95b7f24a1
Daviduo <davide0400@gmail.com>
2022-11-23 15:58:43 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,62 @@
+const posts = [
+    {
+        "id": 1,
+        "descr": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "utente": {
+            "nome": "Mario Rossi",
+            "imgUtente": "https://unsplash.it/300/300?image=15"
+        },
+        "miPiace": 80,
+        "data": "mm/gg/yyyy"
+    },
+    {
+        "id": 2,
+        "descr": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "utente": {
+            "nome": "Roberto Verdi",
+            "imgUtente": "https://unsplash.it/300/300?image=15"
+        },
+        "miPiace": 190,
+        "data": "mm/gg/yyyy"
+    },
+    {
+        "id": 3,
+        "descr": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "utente": {
+            "nome": "Gianna nannini",
+            "imgUtente": "https://unsplash.it/300/300?image=15"
+        },
+        "miPiace": 145,
+        "data": "mm/gg/yyyy"
+    },
+    {
+        "id": 4,
+        "descr": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "",
+        "utente": {
+            "nome": "Valeria Valentini",
+            "imgUtente": "https://unsplash.it/300/300?image=15"
+        },
+        "miPiace": 53,
+        "data": "mm/gg/yyyy"
+    },
+    {
+        "id": 5,
+        "descr": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "utente": {
+            "nome": "Riccardo Zanotti",
+            "imgUtente": "https://unsplash.it/300/300?image=15"
+        },
+        "miPiace": 77,
+        "data": "mm/gg/yyyy"
+    },
+];
+
+posts.forEach((Element) => {
+    console.log(Element)
+
+});
